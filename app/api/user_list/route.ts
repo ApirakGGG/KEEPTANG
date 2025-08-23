7f9caadd5d416e499349fc06b82075a840dd014d
ApirakGGG <naratah012345@gmail.com>
2025-08-24 01:34:57 +0700

    third commit

--- app/api/user_list/route.ts
+++ app/api/user_list/route.ts
@@ -1,8 +1,7 @@
 import { clerkClient, currentUser } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-
-export async function GET() : Promise<Response> {
+export async function GET(): Promise<Response> {
   try {
     const clerk = await clerkClient();
     const user = await currentUser();
@@ -10,11 +9,11 @@ export async function GET() : Promise<Response> {
       return NextResponse.json({ error: "No user found" }, { status: 404 });
     }
 
-     const usersList = await clerk.users.getUserList({
+    const usersList = await clerk.users.getUserList({
       limit: 50,
     });
     // console.log("User_List", usersList);
-    return NextResponse.json(usersList) as unknown as Response;
+    return NextResponse.json(usersList as unknown);
   } catch (err) {
     console.error("error", err);
     return NextResponse.json({ err: "Failed to fetch users" }, { status: 500 });
