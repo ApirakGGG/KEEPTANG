fe8eeb4d7e756d2875ade29711b798300d54031c
ApirakGGG <naratah012345@gmail.com>
2025-08-24 01:58:23 +0700

    third cm

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,6 @@
 export const rules = {
-  "@typescript-eslint/no-unused-vars": process.env.NODE_ENV === "production" ? "error" : "warn",
+  "@typescript-eslint/no-unused-vars":
+    process.env.NODE_ENV === "production" ? "error" : "warn",
+  " eslint-disable @typescript-eslint/no-explicit-any":
+    process.env.NODE_ENV === "production" ? "error" : "warn",
 };
